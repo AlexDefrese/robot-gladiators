c07711d24ad06202a7d4b0c1b41ce36ace8c0d05
Alex Defrese <aidandefrese@gmail.com>
2022-08-11 11:48:57 -0400

    added enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,22 +1,35 @@
+// Game states
+// "WIN" - Player robot has defeated all enemy robots
+//         *Fight all enemy robots
+//          *Defeat all enemy robots
+//  "Lose" - Player robot has zero or less health 
+
+
+
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
+var enemyNames = ["Roborto", "Amy Andriod", "Robo Trumble"];
+var enemyHealth = 50;
+var enemyAttack = 12;
 // you can log  multiple values at once like this
  console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
-var enemyHealth = 50;
-var enemyAttack = 12;
+for(var i=0; i < enemyNames.length; i++) {
+    console.log(enemyNames[i]);
+    console.log(i);
+    console.log(enemyNames[i] + " is at " + i + " index");
+}
 
-var fight = function() {
+var fight = function(enemyName) {
     // Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT or 'SKIP' to choose.")
 
     // if player chooses to fight, then fight
-if(promptFight==="fight" || promptFight ==="FIGHT"){
+if(promptFight=== "fight" || promptFight === "FIGHT"){
     
     // Subtract the value of the 'playerAttack' from the value of 'enemyHealth' and us that result to update the value in the 'enemyHealth' varible
     enemyHealth = enemyHealth - playerAttack;
@@ -50,7 +63,7 @@ if(promptFight==="fight" || promptFight ==="FIGHT"){
     }
     
     // if player chooses to skip
-    } else if (promptFight ==="skip" || promptFight === "SKIP") {
+    } else if (promptFight === "skip" || promptFight === "SKIP") {
         // confirm if player wants to skip
         var confirmSkip = window.confirm("Are you sure you'd like to quit?");
         // if yes (true), leave fight
@@ -65,5 +78,6 @@ if(promptFight==="fight" || promptFight ==="FIGHT"){
         }
 }
 }
-
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
